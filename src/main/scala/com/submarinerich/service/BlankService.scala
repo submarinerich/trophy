4ac16fbfbabc21d1bcff2a581d737fb5f52db938
C. Anderson Miller <anderson@submarinerich.com>
2011-11-23 17:37:16 -0500

    first commit!

---
+++ src/main/scala/com/submarinerich/service/BlankService.scala
@@ -0,0 +1,60 @@
+package com.submarinerich.service
+
+import unfiltered.request._
+import unfiltered.response._
+
+import org.slf4j.{LoggerFactory,Logger}
+import org.squeryl.SessionFactory
+
+/** unfiltered plan */
+class BlankService extends unfiltered.filter.Plan {
+  import QParams._
+
+  val logger = LoggerFactory.getLogger(getClass())
+
+  def intent = {
+    case GET(Path("/")) =>
+      logger.debug("GET /")
+      Ok ~> view(Map.empty)(<p> What say you? </p>)
+    case POST(Path("/") & Params(params)) =>
+      logger.debug("POST /")
+      val vw = view(params)_
+      val expected = for {
+        int <- lookup("int") is
+          int { _ + " is not an integer" } is
+          required("missing int")
+        word <- lookup("palindrome") is
+          trimmed is
+          nonempty("Palindrome is empty") is
+          pred(palindrome, { _ + " is not a palindrome" }) is
+          required("missing palindrome")
+      } yield vw(<p>Yup. { int.get } is an integer and { word.get } is a palindrome. </p>)
+      expected(params) orFail { fails =>
+        vw(<ul> { fails.map { f => <li>{f.error} </li> } } </ul>)
+      }
+  }
+  def palindrome(s: String) = s.toLowerCase.reverse == s.toLowerCase
+  def view(params: Map[String, Seq[String]])(body: scala.xml.NodeSeq) = {
+    def p(k: String) = params.get(k).flatMap { _.headOption } getOrElse("")
+    Html(
+     <html>
+      <head>
+        <title>uf example</title>
+        <link rel="stylesheet" type="text/css" href="/css/app.css"/>
+      </head>
+      <body>
+       <div id="container">
+       { body }
+       <form method="POST">
+         <div>Integer <input type="text" name="int" value={ p("int") } /></div>
+         <div>Palindrome <input type="text" name="palindrome" value={ p("palindrome") } /></div>
+         <input type="submit" />
+       </form>
+       </div>
+     </body>
+    </html>
+   )
+  }
+}
+
+// vim: set ts=4 sw=4 et:
